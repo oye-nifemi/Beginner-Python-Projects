a4351bfc876357dbff0c5cdbdd36e019e32873a1
oye-nifemi <nifoyewole@gmail.com>
2022-11-29 12:32:03 +0100

    word replacement

---
+++ word_replacement.py
@@ -0,0 +1,8 @@
+def replace():
+
+    str = "Hi, I am Nifemi, and hi hi hi hi"
+    word_to_replace = input("Enter the word to replce:")
+    replacement = input("Enter the word replacement: ")
+
+    print(str.replace(word_to_replace,replacement))
+replace()
